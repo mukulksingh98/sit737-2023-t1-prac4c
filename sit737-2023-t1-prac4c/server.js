6035736b6ab1493e2039b6fda8c64fa9666b76f1
mukulksingh98 <109273622+mukulksingh98@users.noreply.github.com>
2023-04-09 16:52:25 +1000

    Update

--- sit737-2023-t1-prac4c/server.js
+++ sit737-2023-t1-prac4c/server.js
@@ -9,19 +9,13 @@ const logger = winston.createLogger({
     format: winston.format.json(),
     defaultMeta: { service: 'calculate-service' },
     transports: [
-      //
-      // - Write all logs with importance level of `error` or less to `error.log`
-      // - Write all logs with importance level of `info` or less to `combined.log`
-      //
+      
       new winston.transports.File({ filename: 'error.log', level: 'error' }),
       new winston.transports.File({ filename: 'combined.log' }),
     ],
   });
   
-  //
-  // If we're not in production then log to the `console` with the format:
-  // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-  //
+  
   if (process.env.NODE_ENV !== 'production') {
     logger.add(new winston.transports.Console({
       format: winston.format.simple(),
@@ -202,7 +196,7 @@ app.get('/', function (req, res) {
 });
 
 
-const port=3040;
+const port=3020;
 app.listen(port,()=> {
     console.log("hello i'm listening to port " +port);
 })
